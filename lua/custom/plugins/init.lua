6183f5d3b9aa1ee3dc34c35a0ec53686c1f023f7
Robert Colburn <60761375+RobertColburn@users.noreply.github.com>
2024-06-08 20:36:27 -0500

    Added plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {'rktjmp/lush.nvim',
+{
+'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    end
+}
+}
